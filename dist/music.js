8e62a68c437ba0232907a0cbaf54c724fa508d40
NeNoOD <nenood@outlook.com>
2024-01-22 23:09:22 +0800

    Site updated: 2024-01-22 23:09:15

---
+++ dist/music.js
@@ -0,0 +1,15 @@
+const ap = new APlayer
+  ({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio:
+      [
+        {
+          name: 'Tojita Sekai',
+          artist: 'かめりあ',
+          url: 'http://music.163.com/song/media/outer/url?id=1336689106.mp3',
+          cover: 'https://cdn.jsdelivr.net/gh/nenood/pic_for_blogs@main/img/tojitasekai.jpg',
+        }
+      ]
+  });
